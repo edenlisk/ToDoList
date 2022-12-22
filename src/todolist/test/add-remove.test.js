3e5ff9c38a8681186ec3e037acc9ce76de07bf39
edenlisk <nkumbuyenock@gmail.com>
2022-12-22 20:19:41 +0200

    Fix Eslint Errors

--- src/todolist/test/add-remove.test.js
+++ src/todolist/test/add-remove.test.js
@@ -4,21 +4,21 @@
 import { ToDoListClass } from './script.js';
 
 describe('Testing Add and remove methods', () => {
-    const ToDoList = new ToDoListClass();
-    test('add first item', () => {
-        const description = 'get milk';
-        ToDoList.addToDo(description);
-        const result = JSON.parse(localStorage.getItem('todo'));
-        expect(result.length).toEqual(1);
-    });
-    test('add second item', () => {
-        ToDoList.addToDo('completing assignments');
-        const result = JSON.parse(localStorage.getItem('todo'));
-        expect(result.length).toEqual(2);
-    });
-    test('remove item', () => {
-        ToDoList.removeItem(0);
-        const result = JSON.parse(localStorage.getItem('todo'));
-        expect(result.length).toEqual(1);
-    });
+  const ToDoList = new ToDoListClass();
+  test('add first item', () => {
+    const description = 'get milk';
+    ToDoList.addToDo(description);
+    const result = JSON.parse(localStorage.getItem('todo'));
+    expect(result.length).toEqual(1);
+  });
+  test('add second item', () => {
+    ToDoList.addToDo('completing assignments');
+    const result = JSON.parse(localStorage.getItem('todo'));
+    expect(result.length).toEqual(2);
+  });
+  test('remove item', () => {
+    ToDoList.removeItem(0);
+    const result = JSON.parse(localStorage.getItem('todo'));
+    expect(result.length).toEqual(1);
+  });
 });
